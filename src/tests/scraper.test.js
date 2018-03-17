90178bed6ba9de9e245feebdf1651dab2dd9a139
yaojie <limyaojie93@gmail.com>
2018-03-17 11:06:59 +0800

    clean up test

--- src/tests/scraper.test.js
+++ src/tests/scraper.test.js
@@ -2,9 +2,8 @@ import NightStalker from '../components/NightStalker';
 
 jest.setTimeout(10000);
 test('it should get posts', async () => {
-  const ns = new NightStalker('rrreol999');
+  const ns = new NightStalker('instagram');
   const posts = await ns.getPosts(3);
-  console.log(posts);
   expect(posts).toHaveLength(3);
 });
 
