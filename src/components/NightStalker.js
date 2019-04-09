8855faaea67c41bbe4b7b8e635fa80aa14b0d860
yaojie <limyaojie93@gmail.com>
2019-04-09 11:36:36 +0800

    use puppeteer

--- src/components/NightStalker.js
+++ src/components/NightStalker.js
@@ -1,55 +1,72 @@
 /* eslint-env browser */
 
-import Nightmare from 'nightmare';
 import Grapher from './Grapher';
+
+const puppeteer = require('puppeteer');
 
 export default class NightStalker {
   constructor(username) {
     this.username = username;
-    this.nightmare = Nightmare({ show: false });
   }
 
   async getPostsFrom(graph) {
-    await this.nightmare
-      .goto(`https://www.instagram.com/p/${graph.shortcode}/?taken-by=${this.username}`)
-      .evaluate(() => {
-        // eslint-disable-next-line
-        const post = window._sharedData.entry_data.PostPage[0].graphql.shortcode_media;
-        // eslint-disable-next-line
-        let media = [];
-        if (post.edge_sidecar_to_children) {
-          media = post.edge_sidecar_to_children.edges.map(edge => edge.node.display_resources.slice(-1)[0].src);
-        } else {
-          media = [post.display_resources.slice(-1)[0].src];
-        }
-        const [captionEdge] = post.edge_media_to_caption.edges;
-        const data = {
-          media,
-          description: captionEdge ? captionEdge.node.text : '',
-        };
-        return data;
-      })
-      .then((result) => {
-        graph.setMedia(result.media);
-        graph.setDescription(result.description);
-      });
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(`https://www.instagram.com/p/${graph.shortcode}/?taken-by=${this.username}`);
+
+    const result = await page.evaluate(() => {
+      // eslint-disable-next-line
+      const post = window._sharedData.entry_data.PostPage[0].graphql.shortcode_media;
+      // eslint-disable-next-line
+      let media = [];
+      if (post.edge_sidecar_to_children) {
+        media = post.edge_sidecar_to_children.edges.map(edge => edge.node.display_resources.slice(-1)[0].src);
+      } else {
+        media = [post.display_resources.slice(-1)[0].src];
+      }
+      const [captionEdge] = post.edge_media_to_caption.edges;
+      const data = {
+        media,
+        description: captionEdge ? captionEdge.node.text : '',
+      };
+      return data;
+    });
+    graph.setMedia(result.media);
+    graph.setDescription(result.description);
+
     return graph;
   }
 
   async getPosts(count = 3) {
-    const graphEdges = await this.nightmare
-      .goto(`https://www.instagram.com/${this.username}`)
-      .evaluate((postCount) => {
-        const postArray = [];
-        // eslint-disable-next-line no-underscore-dangle
-        const [profile] = window._sharedData.entry_data.ProfilePage;
-        // eslint-disable-next-line prefer-destructuring
-        const edges = profile.graphql.user.edge_owner_to_timeline_media.edges;
-        edges.slice(0, postCount).forEach((edge) => {
-          postArray.push(edge.node);
-        });
-        return postArray;
-      }, count);
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(`https://www.instagram.com/${this.username}`);
+
+    const graphEdges = await page.evaluate((postCount) => {
+      const postArray = [];
+      // eslint-disable-next-line no-underscore-dangle
+      const [profile] = window._sharedData.entry_data.ProfilePage;
+      // eslint-disable-next-line prefer-destructuring
+      const edges = profile.graphql.user.edge_owner_to_timeline_media.edges;
+      edges.slice(0, postCount).forEach((edge) => {
+        postArray.push(edge.node);
+      });
+      return postArray;
+    }, count);
+
+    // const graphEdges = await this.nightmare
+    //   .goto(`https://www.instagram.com/${this.username}`)
+    //   .evaluate((postCount) => {
+    //     const postArray = [];
+    //     // eslint-disable-next-line no-underscore-dangle
+    //     const [profile] = window._sharedData.entry_data.ProfilePage;
+    //     // eslint-disable-next-line prefer-destructuring
+    //     const edges = profile.graphql.user.edge_owner_to_timeline_media.edges;
+    //     edges.slice(0, postCount).forEach((edge) => {
+    //       postArray.push(edge.node);
+    //     });
+    //     return postArray;
+    //   }, count);
 
     const graphs = Grapher.deconstruct(graphEdges);
     return graphs.reduce((accumulator, graph) =>
@@ -57,10 +74,6 @@ export default class NightStalker {
         this.getPostsFrom(graph).then((updatedGraph) => {
           results.push(updatedGraph);
           return results;
-        })), Promise.resolve([]))
-      .then((results) => {
-        this.nightmare.end().then();
-        return results;
-      });
+        })), Promise.resolve([]));
   }
 }
