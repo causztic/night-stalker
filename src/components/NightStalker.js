ac9fbffc7089b3f32988332c9d0d8de75c813fd6
yaojie <limyaojie93@gmail.com>
2019-10-05 16:39:51 +0800

    upgrade packages

--- src/components/NightStalker.js
+++ src/components/NightStalker.js
@@ -171,6 +171,7 @@ export default class NightStalker {
         const [profile] = window._sharedData.entry_data.ProfilePage;
         // eslint-disable-next-line prefer-destructuring
         const edges = profile.graphql.user.edge_owner_to_timeline_media.edges;
+        console.log(edges);
         edges.slice(0, postCount).forEach((edge) => {
           postArray.push(edge.node);
         });
@@ -187,8 +188,7 @@ export default class NightStalker {
             return results;
           })), Promise.resolve([]));
     } catch (e) {
-      console.error(e);
-      return null;
+      return [];
     }
   }
 }
